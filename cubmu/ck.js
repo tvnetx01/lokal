102a45a2cdef555fb7fb095c4ce802548893dc12
tvnetx01 <tvnetx01@gmail.com>
2025-11-24 23:16:10 +0800

    Update ck.js

--- cubmu/ck.js
+++ cubmu/ck.js
@@ -1,21 +1,22 @@
 {
-  "Trans_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8:cc9cfb1f392e4b4180d79fa8178ab041",
-  "Indosiar": "7863725870545163494c4c7741306f65:327745514751744c35374f3079426232",
+  "api_url": "https://servicebuss.transvision.co.id/cubmu/channel/v2/get_url_transform?type=live_tv&genre_id=10&platform_id=4028c68574537fcd0174af6756a94288&media_ext=MPD",
+  "tokens": {
+    "Trans_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8:cc9cfb1f392e4b4180d79fa8178ab041",
+    "Indosiar": "7863725870545163494c4c7741306f65:327745514751744c35374f3079426232",
     "Trans_7": "3680de59582949ec9ba83a9ca0754997:9c755da9b7a14a85a6a69b0c7eec1880",
-      "MetroTv": "614b55593846676b4f42443249457759:78776537304c574f4b4f6656526b5662",
-      "CNN_Indonesia": "609ee01ee0c7408e93d9096ca9752643:401a98e4abc546498e8a858b5f9f5670",
-      "SCTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-      "RCTI": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "MetroTv": "614b55593846676b4f42443249457759:78776537304c574f4b4f6656526b5662",
+    "CNN_Indonesia": "609ee01ee0c7408e93d9096ca9752643:401a98e4abc546498e8a858b5f9f5670",
+    "SCTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "RCTI": "edf4f4ef3ffa4737b48a29e6c6be63a8",
     "GTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "Jak_tv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "BTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "Kompas_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "BTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "rtv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "MNC_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "TVRI": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "jtv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "Berita_Satu": "edf4f4ef3ffa4737b48a29e6c6be63a8",
-        "Garuda_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8"
-
+    "Jak_tv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "BTV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "Kompas_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "rtv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "MNC_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "TVRI": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "jtv": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "Berita_Satu": "edf4f4ef3ffa4737b48a29e6c6be63a8",
+    "Garuda_TV": "edf4f4ef3ffa4737b48a29e6c6be63a8"
+  }
 }
